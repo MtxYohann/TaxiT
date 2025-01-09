ff8bdfc52baecf9c07399444518719279516ac62
MtxYohann <137070655+Yohann-mtx@users.noreply.github.com>
2025-01-09 16:52:48 +0100

    add navbar, modif page d'accueil

---
+++ src/app/exemple/page.js
@@ -0,0 +1,8 @@
+export default function ExemplePage() {
+    return (
+        <div>
+            <h1>Page Maps</h1>
+            <p>Ceci est une nouvelle page appelée "Maps".</p>
+        </div>
+    );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
+import Navbar from "../components/Navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,9 +19,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
