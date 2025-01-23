01bfdf556f75bec63b794c6a9506380e8d4204ee
MtxYohann <137070655+Yohann-mtx@users.noreply.github.com>
2025-01-23 16:50:58 +0100

    add title/image

--- src/app/page.js
+++ src/app/page.js
@@ -5,20 +5,26 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <div className={styles.ctas}>
-                    <a>Suggestion</a>
-                    <div >
-                        <a className={styles.primary}
-                            href="/maps">
-                            <Image
-                                src="/iconTaxi.png"
-                                alt="Taxi course"
-                                width={50}
-                                height={50}
-                            />
-                            course
-                        </a>
-                        <a className={styles.primary}>Read our docs</a>
+                <h1 className={styles.title}>TaxiT</h1>
+                <div className={styles.container}>
+                    <div>
+                        <Image src="/ville.jpg" alt="Ville" width={650} height={430} className={styles.image} />
+                    </div>
+                    <div className={styles.ctas}>
+                        <a>Suggestion</a>
+                        <div >
+                            <a className={styles.primary}
+                                href="/maps">
+                                <Image
+                                    src="/iconTaxi.png"
+                                    alt="Taxi course"
+                                    width={50}
+                                    height={50}
+                                />
+                                course
+                            </a>
+                            <a className={styles.primary}>Read our docs</a>
+                        </div>
                     </div>
                 </div>
             </main>
