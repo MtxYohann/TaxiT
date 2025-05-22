e105b4523a37255857d1ab5b57648ab345ca4180
Yohann Mathieux <yohann.mathieux@ecole-isitech.fr>
2025-05-22 18:20:57 +0200

    feature : add profile page and delete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
 import { LoginButton, RegisterButton } from "./buttons";
+import { useSession } from "next-auth/react";
 
 export default function Navbar() {
+    const { data: session } = useSession();
     return (
         <nav className={styles.navbar}>
             <ul>
                 <li><Link href="/">Accueil</Link></li>
                 <li><Link href="/about">About</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
+                <li><Link href="/contact">Contact</Link></li>              
                 <li><LoginButton /></li>
                 <li><RegisterButton /></li>
+                {session?.user && (
+                    <li className={styles.profile}><Link href="/account">Profile</Link></li>
+                )}
             </ul>
         </nav>
     );
---
+++ src/utils/accountActions.js
@@ -0,0 +1,47 @@
+// Fonction pour récupérer les données utilisateur
+export const fetchUserData = async (email, setUser, setError) => {
+    try {
+        console.log("email", email);
+        const res = await fetch("http://localhost:4000/api/user", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email }),
+        });
+
+        if (!res.ok) {
+            throw new Error("Impossible de récupérer les informations utilisateur.");
+        }
+
+        const data = await res.json();
+        setUser(data);
+    } catch (err) {
+        setError(err.message);
+    }
+};
+
+// Fonction pour supprimer le compte
+export const deleteAccount = async (router, setError, email) => {
+    if (confirm("Êtes-vous sûr de vouloir supprimer votre compte ?")) {
+        try {
+            const res = await fetch("http://localhost:4000/api/delete", {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email }),
+            });
+
+            if (!res.ok) {
+                throw new Error("Erreur lors de la suppression du compte.");
+            }
+
+            alert("Compte supprimé avec succès.");
+            router.push("/register");
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+};
+
+// Fonction pour modifier les données utilisateur
+export const editAccount = (router) => {
+    router.push("/edit-account"); // Rediriger vers une page d'édition
+};
